ef5c19925cfabd6b8ff2b0907ea11ca7f0b36534
Ahmad Sadeghi <allaboutams@gmail.com>
2023-09-02 10:41:51 +0330

    Revert "Initial commit"
    This reverts commit d790aa2d7e0a0a8cab782451df097b8e4fa9b2fe.

--- server/modules/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB_HOST: 'localhost',
-    DB_USER: 'postgres',
-    DB_NAME: 'postgres',
-    DB_SCHEMA: 'jobs',
-};
--- server/modules/db.js
+++
@@ -1,9 +0,0 @@
-const config = require('./config');
-const pgp = require('pg-promise')();
-const db = pgp(
-    `postgresql://${config.DB_USER}@${config.DB_HOST}:5432/${config.DB_NAME}`
-);
-
-db.none(`SET search_path TO ${config.DB_SCHEMA}`);
-
-module.exports = db;
